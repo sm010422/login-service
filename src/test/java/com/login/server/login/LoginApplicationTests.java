d1bb51025793a72d69596e8f98acce70da26a197
Park Sang Min <sm0104224@gmail.com>
2026-02-22 15:30:29 +0900

    init(login-service): first commit for login-service

---
+++ src/test/java/com/login/server/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.server.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
