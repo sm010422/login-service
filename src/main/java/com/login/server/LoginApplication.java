4572ee0b2760fb4bf809f45b6d7f097f7677cf1a
Park Sang Min <sm0104224@gmail.com>
2026-03-03 04:27:49 +0900

    fix: resolve null constraint violatoin on created_at using JPA Auditing
    - 메인 클래스 @EnableJpaAuditing Annotation 추가
    - Member 엔티티 생성 시 날짜 자동 입력을 위해 BaseEntity 및 리스너 설정
    확인

--- src/main/java/com/login/server/LoginApplication.java
+++ src/main/java/com/login/server/LoginApplication.java
@@ -2,8 +2,12 @@ package com.login.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
+@EnableJpaAuditing
 public class LoginApplication {
 
 	public static void main(String[] args) {
