712eba72a1fa59b209d33b1c1c0c2d4394d12d67
Park Sang Min <sm0104224@gmail.com>
2026-03-03 04:54:21 +0900

    fix: update OAuth2SuccessHandler redirect path for local environment

--- src/main/java/com/login/server/domain/member/security/oauth/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/login/server/domain/member/security/oauth/OAuth2LoginSuccessHandler.java
@@ -46,8 +46,8 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
                 .ifPresentOrElse(auth -> auth.updateToken(refreshToken),
                         () -> authRepository.save(new Auth(member,refreshToken)));
 
-        String redirectUrl = "https://beanba.store/oauth2/callback"
-                +"?accessToken="+ URLEncoder.encode(accessToken, StandardCharsets.UTF_8);
+        String redirectUrl = "/index.html?accessToken="
+                + URLEncoder.encode(accessToken, StandardCharsets.UTF_8);
 
         getRedirectStrategy().sendRedirect(request, response, redirectUrl);
     }
