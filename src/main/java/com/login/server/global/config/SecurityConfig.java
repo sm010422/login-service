b38a1eed7e61e019a47346e87623d3b1af184660
Park Sang Min <sm0104224@gmail.com>
2026-03-03 16:13:15 +0900

    fix(auth): add OAuth3AuthroizationRequestResolver to support custom login prompts
    - SecurityConfig에 OAuth2AuthorizationRequestResolver 커스텀 설정 추가
    - Kakao(prompt=login), Google(select_account), Naver(reprompt) 파라키터
    전달 허용
    - 로그아웃 후 재로그인시 계정 선택창이 항상 뜨도록 수정

--- src/main/java/com/login/server/global/config/SecurityConfig.java
+++ src/main/java/com/login/server/global/config/SecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -33,102 +36,127 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+    
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
-  private final JwtTokenProvider jwtTokenProvider;
-  private final CustomUserDetailsService customUserDetailsService;
-  private final CustomOAuth2UserService customOAuth2UserService;
-  private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable);
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(AbstractHttpConfigurer::disable);
+        http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement((session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
 
-    http
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-        .csrf(csrf -> csrf.disable())
-        .sessionManagement((session -> session
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .requestMatchers("/api/auth/login", "/api/auth/refresh", "/api/auth/signup/**", "/oauth2/**","/login/oauth2/**").permitAll()
+                .requestMatchers("/api/member/findId", "/api/member/findPassword", "/api/member/findPassword/verify").permitAll()
+                .requestMatchers("/v3/api-docs/**","/swagger-ui/**","/swagger-ui.html").permitAll()
+                .requestMatchers(
+                    "/upload",
+                    "/api/test-sale-post/**",
+                    "/api/sale-post/all/**",
+                    "/api/sale-post/detail/**",
+                    "/api/sale-post/top-view/**"
+                ).permitAll()
+                .requestMatchers("/api/health/**").permitAll() //배포 헬스체크
+                .requestMatchers(
+                        "/",
+                        "/index.html",
+                        "/*.html",
+                        "/js/**",
+                        "/api/ws-chat",
+                        "/app/**",
+                        "/api/rooms/**",
+                        "/api/kamis/all",
+                        "/api/sale-post/elasticsearch",
+                        "/api/rooms/**"
 
-    http.authorizeHttpRequests(auth -> auth
-            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-            .requestMatchers("/api/auth/login", "/api/auth/refresh", "/api/auth/signup/**", "/oauth2/**","/login/oauth2/**").permitAll()
-            .requestMatchers("/api/member/findId", "/api/member/findPassword", "/api/member/findPassword/verify").permitAll()
-            .requestMatchers("/v3/api-docs/**","/swagger-ui/**","/swagger-ui.html").permitAll()
-            .requestMatchers(
-                "/upload",
-                "/api/test-sale-post/**",  //sale-post 테스트 하느라고 잠시 넣어놨습니다.
-                "/api/sale-post/all/**",
-                "/api/sale-post/detail/**",
-                "/api/sale-post/top-view/**"
-            ).permitAll()
-            .requestMatchers("/api/health/**").permitAll() //배포 헬스체크
-            .requestMatchers(
-                    "/",
-                    "/index.html",
-                    "/*.html",
-                    "/js/**", // chatting 테스트 하느라고 잠시 넣어놨습니다.
-                    "/api/ws-chat",
-                    "/app/**",
-                    "/api/rooms/**",
-                    "/api/kamis/all",
-                    "/api/sale-post/elasticsearch",
-                    "/api/rooms/**"
-
-            ).permitAll().requestMatchers(
-                    "/api/admin/**",
-                    "/api/report/**",
-                    "/admin/**",
-                            "/css/**",
-                            "/images/**",
-                            "/favicon.ico"
-                    ).permitAll()//0717 김송이 추가
-            .anyRequest().authenticated()
-        )
-            .formLogin(AbstractHttpConfigurer::disable)
-            .httpBasic(AbstractHttpConfigurer::disable)
-            .exceptionHandling(ex -> ex
-                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                ).permitAll().requestMatchers(
+                        "/api/admin/**",
+                        "/api/report/**",
+                        "/admin/**",
+                                "/css/**",
+                                "/images/**",
+                                "/favicon.ico"
+                        ).permitAll()
+                .anyRequest().authenticated()
             )
-            .oauth2Login(oauth2 -> oauth2
-                .userInfoEndpoint(userInfo -> userInfo
-                .userService(customOAuth2UserService))
-            .successHandler(oAuth2LoginSuccessHandler));
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login(oauth2 -> oauth2
+                    .authorizationEndpoint(authorization -> authorization
+                                    .authorizationRequestResolver(authorizationRequestResolver(clientRegistrationRepository)) // 2. 리졸버 연결
+                                )
+                    .userInfoEndpoint(userInfo -> userInfo
+                    .userService(customOAuth2UserService))
+                .successHandler(oAuth2LoginSuccessHandler));
 
-    http.addFilterBefore(
-        new JwtAuthenticationFilter(jwtTokenProvider, customUserDetailsService),
-        UsernamePasswordAuthenticationFilter.class
-    );
+        http.addFilterBefore(
+            new JwtAuthenticationFilter(jwtTokenProvider, customUserDetailsService),
+            UsernamePasswordAuthenticationFilter.class
+        );
 
-    return http.build();
-  }
+        return http.build();
+    }
 
-  @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-    return configuration.getAuthenticationManager();
-  }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
-  @Bean
-  public AuthenticationProvider authenticationProvider() {
-    DaoAuthenticationProvider provider = new DaoAuthenticationProvider(customUserDetailsService);
-    return provider;
-  }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(customUserDetailsService);
+        return provider;
+    }
 
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedOrigins(List.of("http://localhost:8081","https://beanba.store"));
-    config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-    config.setAllowedHeaders(List.of("*"));
-    config.setAllowCredentials(true); // credentials 필요 없는 경우 false
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:8081","https://beanba.store"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // credentials 필요 없는 경우 false
 
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", config);
-    return source;
-  }
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+        // SecurityConfig.java 내부 또는 별도 빈(Bean)으로 등록
+
+    private OAuth2AuthorizationRequestResolver authorizationRequestResolver(
+            ClientRegistrationRepository clientRegistrationRepository) {
+
+        DefaultOAuth2AuthorizationRequestResolver authorizationRequestResolver =
+                new DefaultOAuth2AuthorizationRequestResolver(
+                        clientRegistrationRepository, "/oauth2/authorization");
+
+        authorizationRequestResolver.setAuthorizationRequestCustomizer(customizer -> {
+            customizer.parameters(params -> {
+                // 카카오용 prompt=login 파라미터 전달 허용
+                params.put("prompt", "login");
+                // 구글용 prompt=select_account 파라미터 전달 허용
+                // params.put("prompt", "select_account"); 
+                // 네이버용 auth_type=reprompt 파라미터 전달 허용
+                params.put("auth_type", "reprompt");
+            });
+        });
+
+        return authorizationRequestResolver;
+    }
 }
